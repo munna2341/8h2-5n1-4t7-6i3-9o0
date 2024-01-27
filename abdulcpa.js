1c441f5840a89a91056f524abd7786dda578b722
munna2341 <157907653+munna2341@users.noreply.github.com>
2024-01-27 23:00:29 +0600

    Update and rename abdulcpa.js to munnacpa.js

--- abdulcpa.js
+++
@@ -1,6 +0,0 @@
-var gpl30 = "https://gp30.me";
-var bll30 = "https://bl30.me";
-var robil30 = "https://robi30.me";
-var airtel30 = "https://airtel30.me";
-var gpl300 = "https://gp300.me";
-var bll300 = "https://bl300.me";
