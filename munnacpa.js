442fd5ce6b1903e1cf144d58441aac489cc97831
munna2341 <157907653+munna2341@users.noreply.github.com>
2024-01-27 23:29:22 +0600

    Update munnacpa.js

--- munnacpa.js
+++ munnacpa.js
@@ -1,5 +1,5 @@
 var gpl30 = "https://vobeluxa.com/cl/9b50b6b70bde627b?p1=&p2=&source=instagram&site=munna";
-var bll30 = "https://vobeluxa.com/cl/82c2a4f8e35946bc?p1=&p2=&source=instagram&site=munnae";
+var bll30 = "https://vobeluxa.com/cl/82c2a4f8e35946bc?p1=&p2=&source=instagram&site=munna";
 var robil30 = "https://vobeluxa.com/cl/0e05f1f600c88bdb?p1=&p2=&source=instagram&site=munna";
 var airtel30 = "https://vobeluxa.com/cl/9b50b6b70bde627b?p1=&p2=&source=instagram&site=munna";
 var gpl300 = "https://vobeluxa.com/cl/9b50b6b70bde627b?p1=&p2=&source=instagram&site=munna";
